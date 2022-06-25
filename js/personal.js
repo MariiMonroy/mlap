3f2dfce0009ba075a6cc163b06093de04cb2da74
MariiMonroy <79820895+MariiMonroy@users.noreply.github.com>
2022-06-24 23:19:11 -0500

    Add files via upload

---
+++ js/personal.js
@@ -0,0 +1,51 @@
+/* nav */
+const menuBtn = document.querySelector('.menu-btn')
+        const navlinks =document.querySelector('.nav-links')
+
+        menuBtn.addEventListener('click',()=>{
+        navlinks.classList.toggle('mobile-menu')
+        });
+/* nav final */
+
+/* info tercer cuerpo */
+(function(){
+        const sliders = [...document.querySelectorAll('.testimony__body')];
+        const buttonNext = document.querySelector('#next');
+        const buttonBefore = document.querySelector('#before');
+        let value;   
+        buttonNext.addEventListener('click', ()=>{
+        changePosition(1);
+        });
+        buttonBefore.addEventListener('click', ()=>{
+        changePosition(-1);
+        });
+        const changePosition = (add)=>{
+        const currentTestimony = document.querySelector('.testimony__body--show').dataset.id;
+        value = Number(currentTestimony);
+        value+= add;
+        sliders[Number(currentTestimony)-1].classList.remove('testimony__body--show');
+        if(value === sliders.length+1 || value === 0){
+                value = value === 0 ? sliders.length  : 1;
+        }
+        sliders[value-1].classList.add('testimony__body--show');
+
+        }
+})();
+
+(function(){
+        const titleQuestions = [...document.querySelectorAll('.questions__title')];
+        console.log(titleQuestions)
+        titleQuestions.forEach(question =>{
+        question.addEventListener('click', ()=>{
+                let height = 0;
+                let answer = question.nextElementSibling;
+                let addPadding = question.parentElement.parentElement;
+                addPadding.classList.toggle('questions__padding--add');
+                question.children[0].classList.toggle('questions__arrow--rotate');
+                if(answer.clientHeight === 0){
+                height = answer.scrollHeight;
+                }
+                answer.style.height = `${height}px`;
+        });
+        });
+})();
